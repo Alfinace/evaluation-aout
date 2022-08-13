efb9489b1e86bdf21e6d7fe456d0d561c6f33184
Alfinace Nantenaina <alfinace-akata@goavana.com>
2022-08-13 15:25:24 +0300

    question 1

---
+++ 1.js
@@ -0,0 +1,35 @@
+function isValidHtml(html) {
+	var tr = [];
+	var isValid = true;
+	var htmlSpecificCharRemove = html.replace(/[^a-zA-Z ]/g, " ").substring(1).split(' ').filter(e => e != '');
+	console.log(htmlSpecificCharRemove);
+	for (let i = 0; i < htmlSpecificCharRemove.length; i++) {
+		const char = htmlSpecificCharRemove[i];
+		if (!tr.includes(char)) {
+			tr.push(char);
+		}else{
+			console.log(char);
+			if (htmlSpecificCharRemove[i-1] != char  && !isOccuranrePair(tr, htmlSpecificCharRemove[i-1])) {
+				isValid = false
+			}			
+		}
+		
+	}
+	console.log(tr);
+	document.getElementById('result').innerHTML = isValid
+	return isValid
+}
+function isOccuranrePair(data, el) {
+	var count = 0;
+	for (let i = 0; i < data.length; i++) {
+		const element = data[i];
+		if(element == el){
+			count += 1 
+		}
+	}
+	return count%2 == 0 ? true : false
+}
+
+
+
+
